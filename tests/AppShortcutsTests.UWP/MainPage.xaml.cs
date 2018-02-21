96a10b65dd136f2ec0fa5ccb9a121ba77a8c2701
Aden Earnshaw <aearnshaw@outlook.com>
2018-02-21 02:08:02 +0000

    Adding UWP Test project

---
+++ tests/AppShortcutsTests.UWP/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Platform.UWP;
+
+namespace AppShortcutsTests.UWP
+{
+    public sealed partial class MainPage : WindowsPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new AppShortcutsTests.App());
+        }
+    }
+}
