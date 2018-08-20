5afeaa62f5ecee5c25fd145ffb87697352b60d18
Aden Earnshaw <aearnshaw@outlook.com>
2018-08-20 23:42:51 +0100

    Converting UWP to multiplatform

---
+++ src/Plugin.AppShortcuts/UWP/CustomIconProvider.uwp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Plugin.AppShortcuts.Icons;
+
+namespace Plugin.AppShortcuts.UWP
+{
+    public class CustomIconProvider : IIconProvider
+    {
+        
+
+        public async Task<object> CreatePlatformIcon(IShortcutIcon shortcutIcon)
+        {
+            return await Task.Run(() =>
+            {
+                var uri = $"ms-appx:///{shortcutIcon.IconName}";
+                return new Uri(uri);
+            });
+        }
+    }
+}
---
+++ src/Plugin.AppShortcuts/UWP/EmbeddedIconProvider.uwp.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Plugin.AppShortcuts.Icons;
+
+namespace Plugin.AppShortcuts.UWP
+{
+    public class EmbeddedIconProvider : IIconProvider
+    {
+        private const string DarkIconUriFormat = "icon_{0}_white.png";
+
+        private readonly EmbeddedImageHelper _embeddedImageHelper;
+
+        public EmbeddedIconProvider()
+        {
+            _embeddedImageHelper = new EmbeddedImageHelper();
+        }
+
+        public async Task<object> CreatePlatformIcon(IShortcutIcon shortcutIcon)
+        {
+            var iconFileName = string.Format(DarkIconUriFormat, shortcutIcon.IconName.ToLower());
+
+            var uri = await _embeddedImageHelper.CopyEmbeddedImageToAppData(iconFileName);
+            return uri;
+        }
+    }
+}
