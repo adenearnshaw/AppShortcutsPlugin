2a2f20ee52474d3a4d0cdb4deb179099a435a675
Aden Earnshaw <aearnshaw@outlook.com>
2018-08-22 22:56:49 +0100

    Converting UWP params to JSON

--- src/Plugin.AppShortcuts/UWP/AppShortcuts.uwp.cs
+++ src/Plugin.AppShortcuts/UWP/AppShortcuts.uwp.cs
@@ -6,6 +6,10 @@ using Windows.Foundation.Metadata;
 using Windows.UI.StartScreen;
 using Plugin.AppShortcuts.Icons;
 using Plugin.AppShortcuts.UWP;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace Plugin.AppShortcuts
 {
@@ -13,7 +17,7 @@ namespace Plugin.AppShortcuts
     {
         private readonly IIconProvider _embeddedIconProvider;
         private readonly IIconProvider _customIconProvider;
-        
+
         public AppShortcutsImplementation()
         {
             IsSupportedByCurrentPlatformVersion = ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 2) && JumpList.IsSupported();
@@ -30,7 +34,8 @@ namespace Plugin.AppShortcuts
 
         public async Task AddShortcut(Shortcut shortcut)
         {
-            var jumplistItem = JumpListItem.CreateWithArguments($"{shortcut.ShortcutId}||{shortcut.Uri}", shortcut.Label);
+            var args = GetSerializedArguments(shortcut.ShortcutId, shortcut.Uri);
+            var jumplistItem = JumpListItem.CreateWithArguments(args, shortcut.Label);
             jumplistItem.Description = shortcut.Description;
             jumplistItem.Logo = await GetIconUri(shortcut.Icon);
 
@@ -46,13 +51,13 @@ namespace Plugin.AppShortcuts
 
             var shortcuts = jumplist.Items.Select(i =>
             {
-                var args = i.Arguments.Split(new[] { "||" }, StringSplitOptions.None);
-                var sc = new Shortcut(args[0])
+                var args = DeserializeArguments(i.Arguments);
+                var sc = new Shortcut(args.ShortcutId)
                 {
                     Label = i.DisplayName,
                     Description = i.Description,
                     Icon = new DefaultIcon(),
-                    Uri = args[1]
+                    Uri = args.Uri
                 };
                 return sc;
             }).ToList();
@@ -78,6 +83,59 @@ namespace Plugin.AppShortcuts
                 return await _customIconProvider.CreatePlatformIcon(shortcutIcon) as Uri;
 
             return await _embeddedIconProvider.CreatePlatformIcon(shortcutIcon) as Uri;
+        }
+
+        private string GetSerializedArguments(string shortcutId, string uri)
+        {
+            var args = new JumpListArguments
+            {
+                ShortcutId = shortcutId,
+                Uri = uri
+            };
+
+            using (var ms = new MemoryStream())
+            {
+                var ser = new DataContractJsonSerializer(typeof(JumpListArguments));
+                ser.WriteObject(ms, args);
+                byte[] json = ms.ToArray();
+                return Encoding.UTF8.GetString(json, 0, json.Length);
+            }
+        }
+
+        private JumpListArguments DeserializeArguments(string json)
+        {
+            // Support for backwards compatibility from v0.5
+            if (json.Contains("||"))
+            {
+                var parts = json.Split(new[] {"||"}, StringSplitOptions.RemoveEmptyEntries);
+                return new JumpListArguments(parts[0], parts[1]);
+            }
+
+            var args = new JumpListArguments();
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var ser = new DataContractJsonSerializer(args.GetType());
+                args = ser.ReadObject(ms) as JumpListArguments;
+                return args;
+            }
+        }
+
+        [DataContract]
+        class JumpListArguments
+        {
+            [DataMember]
+            public string ShortcutId { get; set; }
+            [DataMember]
+            public string Uri { get; set; }
+
+            public JumpListArguments()
+            { }
+
+            public JumpListArguments(string shortcutId, string uri)
+            {
+                ShortcutId = shortcutId;
+                Uri = uri;
+            }
         }
     }
 }
