2390d4f8c100ceffb3cf6195b0cfc8b57ef87b3c
Aden Earnshaw <aearnshaw@outlook.com>
2020-04-13 19:37:22 +0100

    Updating public entities with summaries

--- src/Plugin.AppShortcuts/Common/NotSupportedOnDeviceException.cs
+++ src/Plugin.AppShortcuts/Common/NotSupportedOnDeviceException.cs
@@ -2,14 +2,26 @@
 
 namespace Plugin.AppShortcuts
 {
+    /// <summary>
+    /// Feature is not supported by target device's API
+    /// </summary>
     public class NotSupportedOnDeviceException : Exception
     {
+        /// <summary>
+        /// Feature is not supported by target device's API
+        /// </summary>
         public NotSupportedOnDeviceException() : base()
         { }
 
+        /// <summary>
+        /// Feature is not supported by target device's API
+        /// </summary>
         public NotSupportedOnDeviceException(string message) : base(message)
         { }
 
+        /// <summary>
+        /// Feature is not supported by target device's API
+        /// </summary>
         public NotSupportedOnDeviceException(string message, Exception innerException) : base(message, innerException)
         { }
     }
