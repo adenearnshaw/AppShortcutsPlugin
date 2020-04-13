e5e9dfebf8ae78c7e802eeb4477c62d29fd494b0
Aden Earnshaw <aearnshaw@outlook.com>
2020-04-13 19:37:22 +0100

    Updating public entities with summaries

--- src/Plugin.AppShortcuts/Icons/CustomIcon.cs
+++ src/Plugin.AppShortcuts/Icons/CustomIcon.cs
@@ -1,12 +1,22 @@
 ﻿namespace Plugin.AppShortcuts.Icons
 {
+    /// <summary>
+    /// Icon with a custom image as an icon
+    /// </summary>
     public class CustomIcon : IShortcutIcon
     {
+        /// <summary>
+        /// Creates instance of CustomIcon
+        /// </summary>
+        /// <param name="iconFileName">Custom image name</param>
         public CustomIcon(string iconFileName)
         {
             IconName = iconFileName;
         }
 
+        /// <summary>
+        /// Gets the custom image name for the Icon
+        /// </summary>
         public string IconName { get; }
     }
 }
--- src/Plugin.AppShortcuts/Icons/EmbeddedIcon.cs
+++ src/Plugin.AppShortcuts/Icons/EmbeddedIcon.cs
@@ -1,9 +1,15 @@
 ﻿namespace Plugin.AppShortcuts.Icons
 {
+    /// <summary>
+    /// Icon with image from built-in set
+    /// </summary>
     public abstract class EmbeddedIcon : IShortcutIcon
     {
         internal abstract ShortcutIconType IconType { get; }
 
+        /// <summary>
+        /// Icon name for associated embedded icon
+        /// </summary>
         public string IconName => IconType.ToString();
     }
 }
